d2a2ff5ad8ebd92f83b8d2a180d067f24b15a806
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-05-11 17:34:28 +0800

    Added further logic

--- MinesweeperLib/MinesweeperLib.cs
+++ MinesweeperLib/MinesweeperLib.cs
@@ -6,7 +6,42 @@ namespace MinesweeperLib
     {
         public static char[,] RevealBoard(char[,] inputBoard)
         {
-            return inputBoard;
+            char[,] revealedBoard = inputBoard;
+
+            for (int col = 0; col < revealedBoard.GetLength(0); col++)
+                for (int row = 0; row < revealedBoard.GetLength(1); row++)
+                {
+                    ValidateElement(revealedBoard[col, row]);
+
+                    if (revealedBoard[col, row] == '*')
+                        IncrementSurroundings(ref revealedBoard, col, row);
+                }
+
+            return revealedBoard;
+        }
+
+        private static void ValidateElement(char element)
+        {
+            if (!".*".Contains(element))
+                throw new Exception($"Invalid element value ({element}). Must be . or * only.");
+        }
+
+        private static void IncrementSurroundings(ref char[,] board, int col, int row)
+        {
+            for (int c = col - 1; c <= col + 1; c++)
+                for (int r = row - 1; r <= row + 1; r++)
+                {
+                    if (c != col && r != row)
+                    {
+                        try
+                        {
+                            if (board[c, r] != '*')
+                                board[c, r] = board[c, r] == '.' ? '1' :
+                                    (Convert.ToInt32(board[c, r].ToString()) + 1).ToString().ToCharArray()[0];
+                        }
+                        catch { };
+                    }
+                }
         }
     }
 }
--- MinesweeperLibTest/MinesweeperLibTest.cs
+++ MinesweeperLibTest/MinesweeperLibTest.cs
@@ -10,9 +10,29 @@ namespace MinesweeperLibTest
         public void RevealBoard_Succeeds_WhenDimensionsAreExpected()
         {
             var inputBoard = new char[2, 3];
+            InitializeEmptyBoard(ref inputBoard);
             var revealedBoard = RevealBoard(inputBoard);
             Assert.Equal(2, revealedBoard.GetLength(0));
             Assert.Equal(3, revealedBoard.GetLength(1));
+        }
+
+        [Fact]
+        public void RevealBoard_Fails_WhenInvalidElementValueExists()
+        {
+            var inputBoard = new char[2, 3];
+            InitializeEmptyBoard(ref inputBoard);
+
+            inputBoard[1, 2] = '1';
+
+            var ex = Assert.Throws<Exception>(() => RevealBoard(inputBoard));
+            Assert.Equal("Invalid element value (1). Must be . or * only.", ex.Message);
+        }
+
+        private void InitializeEmptyBoard(ref char[,] board)
+        {
+            for (int col = 0; col < board.GetLength(0); col++)
+                for (int row = 0; row < board.GetLength(1); row++)
+                    board[col, row] = '.';
         }
     }
 }
